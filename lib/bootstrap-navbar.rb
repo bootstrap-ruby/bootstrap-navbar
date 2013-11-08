c4615dd2090914329d3de473771f4fb268e7ca4c
Manuel Meurer <manuel@krautcomputing.com>
2013-11-08 16:49:32 +0100

    Add Bootstrap version 3.0.2

--- lib/bootstrap-navbar.rb
+++ lib/bootstrap-navbar.rb
@@ -3,7 +3,7 @@ require 'gem_config'
 module BootstrapNavbar
   include GemConfig::Base
 
-  BOOTSTRAP_VERSIONS = %w(3.0.1 3.0.0 2.3.2 2.3.1 2.3.0 2.2.2 2.2.1 2.2.0 2.1.1 2.1.0 2.0.4 2.0.3 2.0.2 2.0.1 2.0.0)
+  BOOTSTRAP_VERSIONS = %w(3.0.2 3.0.1 3.0.0 2.3.2 2.3.1 2.3.0 2.2.2 2.2.1 2.2.0 2.1.1 2.1.0 2.0.4 2.0.3 2.0.2 2.0.1 2.0.0)
 
   with_configuration do
     has :bootstrap_version, classes: String, values: BOOTSTRAP_VERSIONS
