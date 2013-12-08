ef4807499e0a9b5b413441f061a2046e593345c4
Manuel Meurer <manuel@krautcomputing.com>
2013-12-08 15:53:24 +0100

    Move spec helper to spec/support folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
 require 'bootstrap-navbar'
 require 'rspec-html-matchers'
-require_relative 'helpers'
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
-  config.include Helpers
+  config.include ::Helpers
 end
