6faa5500d65bfc814623c8f3322b7398d39b71c4
Manuel Meurer <manuel@krautcomputing.com>
2016-08-10 23:20:48 +0200

    update bootstrap 4 stuff, add specs

--- lib/bootstrap-navbar/helpers/bootstrap4.rb
+++ lib/bootstrap-navbar/helpers/bootstrap4.rb
@@ -1,44 +1,54 @@
 module BootstrapNavbar::Helpers::Bootstrap4
   def navbar(options = {}, &block)
     options = options.dup
-    container = options.key?(:container) ? options.delete(:container) : true
+    container = options.key?(:container) ? options.delete(:container) : false
     wrapper options do
       if container
-        container(container, &block)
+        container(&block)
       else
         capture(&block) if block_given?
       end
     end
   end
 
-  def navbar_header(options = {}, &block)
-    options = options.dup
-    brand, brand_link = options.delete(:brand), options.delete(:brand_link)
-    css_classes = %w(navbar-header).tap do |css_classes|
-      css_classes << options.delete(:class) if options.key?(:class)
-    end
-    attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' ')))
-    prepare_html <<-HTML.chomp!
-<div#{attributes}>
-  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapsable">
-    <span class="sr-only">Toggle navigation</span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-  </button>
-  #{brand_link brand, brand_link unless brand.nil?}
-  #{capture(&block) if block_given?}
-</div>
-HTML
-  end
-
   def navbar_collapse(options = {}, &block)
     options = options.dup
-    css_classes = %w(collapse navbar-collapse).tap do |css_classes|
-      css_classes << options.delete(:class) if options.key?(:class)
+    if options.key?(:toggleable)
+      toggleable = options.delete(:toggleable)
+      toggleable = 'xs' if toggleable == true
     end
-    attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' '), id: 'navbar-collapsable'))
+    css_classes = %w(collapse).tap do |css_classes|
+      css_classes << options.delete(:class) if options.key?(:class)
+      css_classes << "navbar-toggleable-#{toggleable}" if toggleable
+    end
+    toggler_css_classes = %w(navbar-toggler).tap do |css_classes|
+      if toggleable
+        following_grid_size = case toggleable
+        when 'xs' then 'sm'
+        when 'sm' then 'md'
+        when 'md' then 'lg'
+        when 'lg' then 'xl'
+        else
+          fail %(Unexpected "toggleable" parameter: #{toggleable}. Must be "xs", "sm", "md", "lg" or `true` (equals "xs").)
+        end
+        css_classes << "hidden-#{following_grid_size}-up"
+      end
+    end
+    id = options.delete(:id) || 'navbar-collapsable'
+    attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' '), id: id))
+    toggler_attributes = attributes_for_tag(
+      class:             toggler_css_classes.join(' '),
+      type:              'button',
+      'data-toggle'   => 'collapse',
+      'data-target'   => "##{id}",
+      'aria-controls' => id,
+      'aria-expanded' => false,
+      'aria-label'    => 'Toggle navigation'
+    )
     prepare_html <<-HTML.chomp!
+<button#{toggler_attributes}>
+  &#9776;
+</button>
 <div#{attributes}>
   #{capture(&block) if block_given?}
 </div>
@@ -48,7 +58,6 @@ HTML
   def navbar_group(options = {}, &block)
     options = options.dup
     css_classes = %w(nav navbar-nav).tap do |css_classes|
-      css_classes << "navbar-#{options.delete(:align)}" if options.key?(:align)
       css_classes << options.delete(:class) if options.key?(:class)
     end
     attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' ')))
@@ -76,8 +85,11 @@ HTML
         .delete_if { |k, v| v.empty? }
         .merge(list_item_options)
     )
-    link_attributes = attributes_for_tag({ class: link_css_classes.join(' ') }
-                                             .delete_if { |k, v| v.empty? }.merge(link_options))
+    link_attributes = attributes_for_tag(
+      { class: link_css_classes.join(' ') }
+        .delete_if { |k, v| v.empty? }
+        .merge(link_options)
+    )
     prepare_html <<-HTML.chomp!
 <li#{list_item_attributes}>
   <a href="#{url}"#{link_attributes}>
@@ -87,94 +99,20 @@ HTML
 HTML
   end
 
-  def navbar_form(url, options = {}, &block)
-    options = options.dup
-    css_classes = %w(form-inline).tap do |css_classes|
-      css_classes << "pull-xs-#{options.delete(:align)}" if options.key?(:align)
-      css_classes << options.delete(:class) if options.key?(:class)
-    end
-    role = options.delete(:role) || 'form'
-    attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' '), role: role))
-    prepare_html <<-HTML.chomp!
-<form action="#{url}" #{attributes}>
-  #{capture(&block) if block_given?}
-</form>
-HTML
-  end
-
-  def navbar_text(text = nil, options = {}, &block)
-    raise StandardError, 'Please provide either the "text" parameter or a block.' if (text.nil? && !block_given?) || (!text.nil? && block_given?)
-    options = options.dup
-    text ||= capture(&block)
-    css_classes = %w(nav-item).tap do |css_classes|
-      css_classes << options.delete(:class) if options.key?(:class)
-    end
-    attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' ')))
-    prepare_html <<-HTML.chomp!
-<p#{attributes}>#{text}</p>
-HTML
-  end
-
-  def navbar_button(text, options = {})
-    options = options.dup
-    css_classes = %w(btn navbar-btn).tap do |css_classes|
-      css_classes << options.delete(:class) if options.key?(:class)
-    end
-    type = options.delete(:type) || 'button'
-    attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' '), type: type))
-    prepare_html <<-HTML.chomp!
-<button#{attributes}>#{text}</button>
-HTML
-  end
-
-  def navbar_dropdown(text, list_item_options = {}, link_options = {}, &block)
-    list_item_options, link_options = list_item_options.dup, link_options.dup
-    list_item_options[:class] = [list_item_options[:class], 'dropdown'].compact.join(' ')
-    list_item_attributes = attributes_for_tag(list_item_options)
-    link_options[:class] = [link_options[:class], 'dropdown-toggle'].compact.join(' ')
-    link_attributes = attributes_for_tag(link_options)
-    prepare_html <<-HTML.chomp!
-<li#{list_item_attributes}>
-  <a href="#" data-toggle="dropdown"#{link_attributes}>#{text} <b class="caret"></b></a>
-  <ul class="dropdown-menu">
-    #{capture(&block) if block_given?}
-  </ul>
-</li>
-HTML
-  end
-
-  def navbar_dropdown_header(text)
-    prepare_html <<-HTML.chomp!
-<li class="dropdown-header">
-  #{text}
-</li>
-HTML
-  end
-
-  def navbar_dropdown_divider
-    prepare_html <<-HTML.chomp!
-<li class="divider"></li>
-HTML
-  end
-
   private
 
-  def container(type, &block)
-    css_class = 'container'
-    css_class << "-#{type}" if type.is_a?(String)
-    attributes = attributes_for_tag(class: css_class)
+  def container(&block)
     prepare_html <<-HTML.chomp!
-<div#{attributes}>
+<div class="container">
   #{capture(&block) if block_given?}
 </div>
 HTML
   end
 
-  def brand_link(name, url = nil)
-    url ||= '/'
+  def brand_link(text, url = nil)
     prepare_html <<-HTML.chomp!
-<a href="#{url}" class="navbar-brand">
-  #{name}
+<a href="#{url || '/'}" class="navbar-brand">
+  #{text}
 </a>
 HTML
   end
@@ -182,27 +120,16 @@ HTML
   def wrapper(options, &block)
     options = options.dup
     css_classes = %w(navbar).tap do |css_classes|
-      # Inverse is now called dark and light (StarWars?)
-      css_classes << "navbar-#{options.delete(:dark) or options.delete(:inverse) ? 'dark' : 'light'}"
-      # Bootstrap4 need background
-      if options.key? :bg
-        css_classes << "bg-#{options.delete(:bg)}"
-      elsif not options.key? :color
-        css_classes << "bg-primary"
-      end
-      # No more static in B4
-      if options.key?(:fixed) or options.key? options.delete(:static)
-        css_classes << "navbar-fixed-#{options.delete(:fixed) || 'top'}"
-        css_classes << 'navbar-full'
-      elsif options.delete(:full)
-        css_classes << 'navbar-full'
-      end
+      css_classes << "navbar-#{options.key?(:color_scheme) ? options.delete(:color_scheme) : 'dark'}"
+      css_classes << "bg-#{options.delete(:bg) || 'primary'}" unless options[:bg] == false
+      css_classes << "navbar-#{options.delete(:placement)}" if options.key?(:placement)
       css_classes << options.delete(:class) if options.key?(:class)
     end
-    role = options.delete(:role) || 'navigation'
-    attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' '), role: role))
+    brand = brand_link(options[:brand], options[:brand_url]) if options[:brand]
+    attributes = attributes_for_tag(options.reverse_merge(class: css_classes.join(' ')))
     prepare_html <<-HTML.chomp!
 <nav#{attributes}>
+  #{brand}
   #{capture(&block) if block_given?}
 </nav>
 HTML
---
+++ spec/bootstrap-navbar/helpers/bootstrap4_spec.rb
@@ -0,0 +1,161 @@
+require 'spec_helper'
+
+describe BootstrapNavbar::Helpers::Bootstrap4 do
+  before do
+    BootstrapNavbar.configure do |config|
+      config.current_url_method = '"/"'
+      config.bootstrap_version  = '4.0.0'
+    end
+  end
+
+  it 'includes the correct module' do
+    expect(renderer.class.ancestors).to_not include(BootstrapNavbar::Helpers::Bootstrap3)
+    expect(renderer.class.ancestors).to_not include(BootstrapNavbar::Helpers::Bootstrap2)
+    expect(renderer.class.ancestors).to include(BootstrapNavbar::Helpers::Bootstrap4)
+  end
+
+  describe '#navbar' do
+    context 'without parameters' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar { 'foo' }).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary' }, text: /foo/)
+      end
+    end
+
+    context 'with "color_scheme" parameter' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar(color_scheme: 'light')).to have_tag(:nav, with: { class: 'navbar navbar-light bg-primary' })
+        expect(renderer.navbar(color_scheme: 'dark')).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary' })
+      end
+    end
+
+    context 'with "bg" parameter' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar(bg: 'primary')).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary' })
+        expect(renderer.navbar(bg: 'inverse')).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-inverse' })
+        expect(renderer.navbar(bg: false)).to have_tag(:nav, with: { class: 'navbar navbar-dark' }, without: { class: 'bg-primary' })
+      end
+    end
+
+    context 'with "placement" parameter' do
+      it 'generates the correct HTML' do
+        %w(full fixed-top fixed-bottom).each do |placement|
+          expect(renderer.navbar(placement: placement)).to have_tag(:nav, with: { class: "navbar navbar-dark bg-primary navbar-#{placement}" })
+        end
+      end
+    end
+
+    context 'with "container" parameter' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar(container: true)).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary' }) do
+          with_tag :div, with: { class: 'container' }
+        end
+        expect(renderer.navbar(container: false)).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary' }) do
+          without_tag :div, with: { class: 'container' }
+        end
+      end
+    end
+
+    context 'with "class" parameter' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar(class: 'foo')).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary foo' })
+      end
+    end
+
+    context 'with "brand" parameter' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar(brand: 'Huhu')).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary' }) do
+          with_tag :a, with: { class: 'navbar-brand' }, text: /Huhu/
+        end
+        expect(renderer.navbar(brand: false)).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary' }) do
+          without_tag :a, with: { class: 'navbar-brand' }
+        end
+      end
+    end
+
+    context 'with "brand_url" parameter' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar(brand: true, brand_url: '/huhu')).to have_tag(:nav, with: { class: 'navbar navbar-dark bg-primary' }) do
+          with_tag :a, with: { class: 'navbar-brand', href: '/huhu' }
+        end
+      end
+    end
+  end
+
+  describe '#navbar_collapse' do
+    context 'without parameters' do
+      it 'generates the correct HTML' do
+        output = renderer.navbar_collapse { 'foo' }
+        expect(output).to have_tag :div, with: { class: 'collapse', id: 'navbar-collapsable' }, text: /foo/
+        button_attributes = {
+          class:             'navbar-toggler',
+          type:              'button',
+          'data-toggle'   => 'collapse',
+          'data-target'   => "#navbar-collapsable",
+          'aria-controls' => 'navbar-collapsable',
+          'aria-expanded' => false,
+          'aria-label'    => 'Toggle navigation'
+        }
+        expect(output).to have_tag :button, with: button_attributes
+      end
+    end
+
+    context 'with "toggleable" parameter' do
+      it 'generates the correct HTML' do
+        output = renderer.navbar_collapse(toggleable: true) { 'foo' }
+        expect(output).to have_tag :div, with: { class: 'collapse navbar-toggleable-xs', id: 'navbar-collapsable' }, text: /foo/
+        button_attributes = {
+          class:             'navbar-toggler hidden-sm-up',
+          type:              'button',
+          'data-toggle'   => 'collapse',
+          'data-target'   => "#navbar-collapsable",
+          'aria-controls' => 'navbar-collapsable',
+          'aria-expanded' => false,
+          'aria-label'    => 'Toggle navigation'
+        }
+        expect(output).to have_tag :button, with: button_attributes
+      end
+    end
+  end
+
+  describe '#navbar_group' do
+    context 'without parameters' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar_group).to have_tag(:ul, with: { class: 'nav navbar-nav' })
+      end
+    end
+
+    context 'with "class" parameter' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar_group(class: 'foo')).to have_tag(:ul, with: { class: 'nav navbar-nav foo' })
+      end
+    end
+  end
+
+  describe '#navbar_item' do
+    it_behaves_like 'marking the navbar items as active correctly'
+
+    context 'with block' do
+      it 'generates the correct HTML' do
+        expect(renderer.navbar_item('/foo', { class: 'list-item' }, class: 'link') { 'link-text' }).to have_tag(:li, with: { class: 'nav-item list-item' }) do
+          with_tag :a, with: { href: '/foo', class: 'nav-link link' }, text: /link-text/
+        end
+      end
+    end
+
+    context 'without current URL' do
+      it 'generates the correct HTML' do
+        BootstrapNavbar.configuration.current_url_method = '"/foo"'
+        expect(renderer.navbar_item('foo', '/')).to have_tag(:li, without: { class: 'active' }) do
+          with_tag :a, with: { href: '/' }, text: /foo/
+        end
+        expect(renderer.navbar_item('foo', '/bar')).to have_tag(:li, without: { class: 'active' }) do
+          with_tag :a, with: { href: '/bar' }, text: /foo/
+        end
+        BootstrapNavbar.configuration.current_url_method = '"/"'
+        expect(renderer.navbar_item('foo', '/foo')).to have_tag(:li, without: { class: 'active' }) do
+          with_tag :a, with: { href: '/foo' }, text: /foo/
+        end
+      end
+    end
+  end
+end
