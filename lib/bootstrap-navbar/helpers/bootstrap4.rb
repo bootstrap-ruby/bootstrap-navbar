b5489f4a0c691d20946abfa5d48c8b87e89cc622
Sebastian Menhofer <sebastian.menhofer@gmail.com>
2018-02-05 16:06:17 +0100

    Add navbar-dropdown-item helper

--- lib/bootstrap-navbar/helpers/bootstrap4.rb
+++ lib/bootstrap-navbar/helpers/bootstrap4.rb
@@ -84,6 +84,20 @@ HTML
 HTML
   end
 
+  def navbar_dropdown_item(text, url = nil, link_options = nil, &block)
+    url ||= '#'
+    link_options = link_options ? link_options.dup : {}
+    link_options[:class] = [link_options[:class], 'dropdown-item'].compact
+    link_options[:class] << 'active' if current_url_or_sub_url?(url)
+    link_options[:class]  = link_options[:class].join(' ')
+    link_attributes = attributes_for_tag(link_options)
+    prepare_html <<~HTML.chomp!
+      <a href="#{url}"#{link_attributes}>
+        #{text}
+      </a>
+    HTML
+  end
+
   private
 
   def container(&block)
