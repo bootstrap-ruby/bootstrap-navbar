607fbef723a9fc9a2aaef2eccc25dfa79fcf17be
Manuel Meurer <manuel@meurer.io>
2023-11-12 15:59:32 +0100

    load bootstrap version from node package config if possible

--- lib/bootstrap-navbar/helpers.rb
+++ lib/bootstrap-navbar/helpers.rb
@@ -1,13 +1,19 @@
 module BootstrapNavbar::Helpers
   def self.included(base)
-    if BootstrapNavbar.configuration.bootstrap_version.nil?
-      unless bootstrap_gem = %w(bootstrap bootstrap-sass).detect { |gem| Gem.loaded_specs.keys.include?(gem) }
-        raise 'Bootstrap version is not configured.'
+    BootstrapNavbar.configuration.bootstrap_version ||= begin
+      node_package_config = "node_modules/bootstrap/package.json"
+
+      case
+      when bootstrap_gem = %w(bootstrap bootstrap-sass).detect { |gem| Gem.loaded_specs.keys.include?(gem) }
+        bootstrap_gem_version = Gem.loaded_specs[bootstrap_gem].version
+        bootstrap_gem_version.segments.take(3).join('.')
+      when File.exist?(node_package_config)
+        JSON.load(File.read(node_package_config)).fetch("version")
+      else
+        raise "Could not determine Bootstrap version from gems or Node package."
       end
-      bootstrap_gem_version = Gem.loaded_specs[bootstrap_gem].version
-      bootstrap_version = bootstrap_gem_version.segments.take(3).join('.')
-      BootstrapNavbar.configuration.bootstrap_version = bootstrap_version
     end
+
     helper_version = BootstrapNavbar.configuration.bootstrap_version[0]
     base.send :include, const_get("Bootstrap#{helper_version}")
   end
